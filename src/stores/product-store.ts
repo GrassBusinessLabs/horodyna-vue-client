8f4d7926f991341bb3c0b5704eaf83f12ceeb7f8
Bezvikonnyy_Maksym_34 <elonmaks60@gmail.com>
2023-12-12 15:32:26 +0200

    Basket

---
+++ src/stores/product-store.ts
@@ -0,0 +1,11 @@
+import {defineStore} from 'pinia'
+interface State {
+   basket: any
+}
+export const productStore=defineStore("productStore",{
+   state:():State =>({
+      basket:[]
+   }),
+
+
+})
