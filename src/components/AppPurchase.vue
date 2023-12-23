456daa73e1bd0b5d29f36324aac2a80cc25c73e5
group35_paklin_david <davidpaklin2004@gmail.com>
2023-12-23 23:58:09 +0200

    Rework: purchase design and functionality

--- src/components/AppPurchase.vue
+++ src/components/AppPurchase.vue
@@ -1,19 +1,22 @@
 <template>
    <v-list-item
-      class="pa-6 rounded-xl bg-white mb-5"
+      class="pa-5 py-4 rounded-xl bg-white mb-5"
       @click='detailsHandler(purchase)'
    >
       <template v-slot:prepend>
          <v-badge color='indigo' :content='purchase.products.length'>
-            <v-avatar size="58" :image="purchase.products[0].img"></v-avatar>
+            <v-avatar size="70" :image="purchase.products[0].img"></v-avatar>
          </v-badge>
       </template>
-      <v-list-item-title class='text-h6 py-1'>
+      <v-list-item-title class='py-1 my-font-size'>
          Покупка #{{ purchase.purchaseId }}
       </v-list-item-title>
       <v-list-item-subtitle class='text-subtitle-1 py-1'>
          {{ purchase.date }}
       </v-list-item-subtitle>
+      <v-list-item-title class='pt-1 my-color my-font-size'>
+         Сума: {{ purchase.products.reduce((accumulator, currentValue) => accumulator + currentValue.price * currentValue.quantity, 0) }}.00 грн
+      </v-list-item-title>
       <template v-slot:append >
          <v-icon icon="mdi-information-outline" size='35'></v-icon>
       </template>
@@ -59,5 +62,13 @@ function detailsHandler(event: Purchase): void {
 
 p {
    font-size: 3.8vw;
+}
+
+.my-font-size {
+   font-size: 17.8px;
+}
+
+.my-color {
+   color: #000099;
 }
 </style>
